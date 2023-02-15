36759e636c5337218d3a2e3c4649cb1aa3013d44
ag91 <andrea-dev@hotmail.com>
2023-02-15 17:51:19 +0000

    show cloc command

--- code-compass.el
+++ code-compass.el
@@ -374,7 +374,10 @@
 
 (defun c/produce-cloc-report (repository)
   "Create cloc report for REPOSITORY. To filter specific subdirectories out of this report, edit the variable `c/exclude-directories'."
-  (message "Producing cloc report...")
+  (message (concat
+            "Producing cloc report with "
+            (format "(cd %s; PERL_BADLANG=0 cloc ./ --by-file --csv --quiet --exclude-dir=%s) > cloc.csv" repository (string-join c/exclude-directories ","))
+            "..."))
   (shell-command
    (format "(cd %s; PERL_BADLANG=0 cloc ./ --by-file --csv --quiet --exclude-dir=%s) > cloc.csv" repository (string-join c/exclude-directories ","))
    nil
