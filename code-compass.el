a68b76f315d5bf46270ff357c621e5871a88be93
Gopar <gopardaniel@gmail.com>
2023-03-25 15:08:57 -0500

    Use built in functions instead of `f` dependency

--- code-compass.el
+++ code-compass.el
@@ -4,7 +4,7 @@
 
 ;; Author: Andrea <andrea-dev@hotmail.com>
 ;; Version: 0.1.3
-;; Package-Requires: ((emacs "26.1") (s "1.12.0") (dash "2.13") (async "1.9.7") (simple-httpd "1.5.1") (f "0.20"))
+;; Package-Requires: ((emacs "26.1") (s "1.12.0") (dash "2.13") (async "1.9.7") (simple-httpd "1.5.1"))
 ;; Keywords: tools, extensions, help
 ;; Homepage: https://github.com/ag91/code-compass
 
@@ -1739,15 +1739,10 @@ Starting DATE reduces scope of Git log.
   "Show in minibuffer the main contributors of this file."
   (interactive)
   (when (and code-compass-display-file-contributors (buffer-file-name))
-    (let ((file (buffer-file-name))
-          (parent-dir nil)
-          (common-parent nil))
+    (let ((file-path buffer-file-name))
       (when (vc-root-dir)
-        (setq parent-dir (f-parent (vc-root-dir)))
-        (setq common-parent (f-common-parent `(,buffer-file-name ,parent-dir)))
-        (setq file (f-relative buffer-file-name common-parent)))
-
-      (message "Contributors of %s:\n%s" file (code-compass--contributors-list-for-current-buffer)))))
+        (setq file-path (file-relative-name (buffer-file-name) (file-name-parent-directory (vc-root-dir)))))
+      (message "Contributors of %s:\n%s" file-path (code-compass--contributors-list-for-current-buffer)))))
 (define-obsolete-function-alias 'c/display-contributors #'code-compass-display-contributors "0.1.2")
 
 (defun code-compass-display-contributors-delayed ()
