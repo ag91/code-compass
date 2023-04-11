190fe9308d35fd32556fe6cdd90a187e165150a6
ag91 <agiugliano@live.it>
2023-04-11 11:13:12 +0100

    fix package docs

--- code-compass.el
+++ code-compass.el
@@ -1,4 +1,4 @@
-;;; code-compass.el --- helps you navigate your complex software aided by metrics and visualization -*- lexical-binding: t; -*-
+;;; code-compass.el --- Navigate software aided by metrics and visualization -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2023 Andrea
 
@@ -26,11 +26,14 @@
 ;; Make Emacs your compass in a sea of software complexity.
 ;;
 ;; This tool puts the power and knowledge of your repository history in your hands.
-;; The current analyses supported are:
-;;   - code-compass-show-hotspots:
+;; You can find what analyses are supported with `code-compass-cheatsheet'.
+;; A good way to start is:
+;;   - `code-compass-show-hotspots':
 ;;     show hotspots in code repository as a circle diagram.
 ;;     Circles are packages or modules.
 ;;     The redder the circle, the more it has been modified lately. The bigger the more code it contains.
+;;
+;; If you are having trouble with dependencies, try `code-compass-doctor' to get some clarity.
 ;;
 ;; See documentation at https://github.com/ag91/code-compass
 
